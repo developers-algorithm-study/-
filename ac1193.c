275aee346aea3039ca7de5a89dcc5a8273bec5ff
kimjochi <67910216+kimjochi@users.noreply.github.com>
2020-07-27 18:32:24 +0900

    Create ac1193.c

---
+++ ac1193.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+	int n, i, x = 1, tmp = 0, cnt, pre = 0, post = 0, order;
+	scanf("%d", &n);
+	for (i = 1; ; i++) {
+		tmp += i;
+		if (tmp >= n) {
+			cnt = i;
+			break;
+		}
+	}
+	for (i = cnt; i > tmp - n; i--) {
+		if (cnt % 2 == 0) {
+			pre = x++;
+			post = i;
+		}
+		else {
+			pre = i;
+			post = x++;
+		}
+	}
+	printf("%d/%d", pre, post);
+	return 0;
+}
